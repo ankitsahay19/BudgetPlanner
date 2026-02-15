0a69885591436cee355f6e9ef995e78ce12d70dc
AnkitSahay <ankitsahay1990@gmail.com>
2026-02-15 22:47:01 +0530

    Added basic CRUD for batch, student & Batchstudents

---
+++ BpstEdu/Areas/Admin/Controllers/BatchStudentsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BpstEdu.DBModels.Student;
+using BpstEdu.Data;
+
+namespace BpstEdu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BatchStudentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BatchStudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BatchStudents
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.BatchStudents.Include(b => b.Batch).Include(b => b.Student);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/BatchStudents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchStudent = await _context.BatchStudents
+                .Include(b => b.Batch)
+                .Include(b => b.Student)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (batchStudent == null)
+            {
+                return NotFound();
+            }
+
+            return View(batchStudent);
+        }
+
+        // GET: Admin/BatchStudents/Create
+        public IActionResult Create()
+        {
+            ViewData["BatchId"] = new SelectList(_context.Batches, "UniqueId", "UniqueId");
+            ViewData["StudentId"] = new SelectList(_context.Students, "UniqueId", "AadhaarNumber");
+            return View();
+        }
+
+        // POST: Admin/BatchStudents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UniqueId,BatchId,StudentId,DiscountedFeeAmount,CreatedDate,LastUpdatedDate")] BatchStudent batchStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(batchStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BatchId"] = new SelectList(_context.Batches, "UniqueId", "UniqueId", batchStudent.BatchId);
+            ViewData["StudentId"] = new SelectList(_context.Students, "UniqueId", "AadhaarNumber", batchStudent.StudentId);
+            return View(batchStudent);
+        }
+
+        // GET: Admin/BatchStudents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchStudent = await _context.BatchStudents.FindAsync(id);
+            if (batchStudent == null)
+            {
+                return NotFound();
+            }
+            ViewData["BatchId"] = new SelectList(_context.Batches, "UniqueId", "UniqueId", batchStudent.BatchId);
+            ViewData["StudentId"] = new SelectList(_context.Students, "UniqueId", "AadhaarNumber", batchStudent.StudentId);
+            return View(batchStudent);
+        }
+
+        // POST: Admin/BatchStudents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UniqueId,BatchId,StudentId,DiscountedFeeAmount,CreatedDate,LastUpdatedDate")] BatchStudent batchStudent)
+        {
+            if (id != batchStudent.UniqueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(batchStudent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BatchStudentExists(batchStudent.UniqueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BatchId"] = new SelectList(_context.Batches, "UniqueId", "UniqueId", batchStudent.BatchId);
+            ViewData["StudentId"] = new SelectList(_context.Students, "UniqueId", "AadhaarNumber", batchStudent.StudentId);
+            return View(batchStudent);
+        }
+
+        // GET: Admin/BatchStudents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchStudent = await _context.BatchStudents
+                .Include(b => b.Batch)
+                .Include(b => b.Student)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (batchStudent == null)
+            {
+                return NotFound();
+            }
+
+            return View(batchStudent);
+        }
+
+        // POST: Admin/BatchStudents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var batchStudent = await _context.BatchStudents.FindAsync(id);
+            if (batchStudent != null)
+            {
+                _context.BatchStudents.Remove(batchStudent);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BatchStudentExists(int id)
+        {
+            return _context.BatchStudents.Any(e => e.UniqueId == id);
+        }
+    }
+}
---
+++ BpstEdu/Areas/Admin/Controllers/StudentsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BpstEdu.DBModels.Student;
+using BpstEdu.Data;
+
+namespace BpstEdu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StudentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Students
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Students.Include(s => s.Application);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Application)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Admin/Students/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "UniqueId", "FirstName");
+            return View();
+        }
+
+        // POST: Admin/Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UniqueId,ApplicationId,FirstName,LastName,Gender,DateOfBirth,Address,CollegeName,MobileNumber,EmailId,HighestQualification,Message,PhotoPath,AadhaarNumber,AadharFileUrl,PanNumber,PanFileUrl")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "UniqueId", "FirstName", student.ApplicationId);
+            return View(student);
+        }
+
+        // GET: Admin/Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "UniqueId", "FirstName", student.ApplicationId);
+            return View(student);
+        }
+
+        // POST: Admin/Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UniqueId,ApplicationId,FirstName,LastName,Gender,DateOfBirth,Address,CollegeName,MobileNumber,EmailId,HighestQualification,Message,PhotoPath,AadhaarNumber,AadharFileUrl,PanNumber,PanFileUrl")] Student student)
+        {
+            if (id != student.UniqueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.UniqueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationId"] = new SelectList(_context.Applications, "UniqueId", "FirstName", student.ApplicationId);
+            return View(student);
+        }
+
+        // GET: Admin/Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Application)
+                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Admin/Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.UniqueId == id);
+        }
+    }
+}
